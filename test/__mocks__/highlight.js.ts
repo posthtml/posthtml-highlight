177648bc1758f1885f35b88ac1a3dd8d2241667a
Casey Webb <notcaseywebb@gmail.com>
2019-08-27 12:08:01 -0500

    chore: fix linter error

--- test/__mocks__/highlight.js.ts
+++ test/__mocks__/highlight.js.ts
@@ -4,7 +4,7 @@ export const configure = jest.fn(hljs.configure)
 export const highlight = jest.fn(hljs.highlight)
 export const highlightAuto = jest.fn(hljs.highlightAuto)
 
-export function mockClear() {
+export function mockClear(): void {
   configure.mockClear()
   highlight.mockClear()
   highlightAuto.mockClear()
